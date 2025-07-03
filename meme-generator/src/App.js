02f03e69de1bb7a1b0b97a263d2c6ed145238f39
Whart0n <code@Matts-MacBook-Pro.local>
2025-07-02 23:04:17 -0600

    Add missing App.js component for React build

---
+++ meme-generator/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>PV Meme Generator</h1>
+        <p>The frontend is live!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
